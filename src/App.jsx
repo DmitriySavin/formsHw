42a8223130b0808c51f6a56556389f7e0bb8aa64
DmitriySavin <debilv556@gmail.com>
2025-12-08 00:54:01 +0200

    фвв

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import "./App.css";
+import { TextInput } from "./components/TextInput/TextInput";
+
+
+export class App extends Component {
+  render() {
+    return (
+    
+    <>
+    <TextInput />
+      </>
+    )
+  }
+}
