64cdf5956b7ee4e25f5c41c0697d269d1161fe8a
DmitriySavin <debilv556@gmail.com>
2026-01-26 13:39:34 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ export class App extends Component {
       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
       { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ];
-    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+    const savedContacts = JSON.parse(localStorage.getItem("contacts") || "[]");
     this.setState({
       contacts: savedContacts.length ? savedContacts : defaultContacts,
     });
